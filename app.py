757631536d58c5af03413e55edfe536d42be51b9
Ondřej Brosch <ondra.brosch.ob@gmail.com>
2026-01-03 23:20:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -604,7 +604,7 @@ for tyden in month_days:
                                     
                                     # POKUD JE ŠEDÝ (is_gray), DÁME MU VELKÝ SPODNÍ PADDING
                                     # POKUD JE BÍLÝ, DÁME MU O 10px MENŠÍ HORNÍ PADDING (0px)
-                                    padding_style = "10px 5px 30px 5px !important" if is_gray else "0px 5px 10px 5px !important"
+                                    padding_style = "10px 5px 28px 5px !important" if is_gray else "0px 5px 10px 5px !important"
                                     
                                     with stylable_container(
                                         key=f"row_{unique_key}_{idx}",
