1e3976736f6bcb536eee11871f2fc3776d120582
Ondřej Brosch <ondra.brosch.ob@gmail.com>
2026-01-09 19:52:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -294,7 +294,7 @@ def vykreslit_detail_akce(akce, unique_key):
 
                                     data_manager.refresh_prihlasky()
                                     
-                                    with utils.st_lottie_spinner(lottie_success, key=f"anim_{unique_key}"): time.sleep(2)
+                                    with styles.st_lottie_spinner(lottie_success, key=f"anim_{unique_key}"): time.sleep(2)
                                     st.toast(f"✅ {finalni_jmeno} zapsán(a)!")
                                     st.rerun()
                             except Exception as e: st.error(f"Chyba: {e}")
