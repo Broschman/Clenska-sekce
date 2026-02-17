b483a08e1fa5ead25c983d22bf7e5614c53dd87d
Ondřej Brosch <ondra.brosch.ob@gmail.com>
2026-02-17 13:41:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -263,7 +263,7 @@ def vykreslit_detail_akce(akce, unique_key):
                         odeslat_btn = st.form_submit_button("Zapsat se", type="primary", use_container_width=True)
                         
                     with c_btn_doprava:
-                        doprava_btn = st.form_submit_button("🚗 Doprava (Dialog)", use_container_width=True)
+                        doprava_btn = st.form_submit_button("🚗 Doprava", use_container_width=True)
                     
                     # --- LOGIKA ODESLÁNÍ ---
                     if odeslat_btn:
